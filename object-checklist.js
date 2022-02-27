10acf463be311095364fe214d9011dc49736b809
Proloycb <proloychacroborty@gmail.com>
2022-02-27 21:58:59 +0600

    second commit

---
+++ object-checklist.js
@@ -0,0 +1,70 @@
+// ১. একটা অবজেক্ট ডিক্লেয়ার করো। সেই অবজেক্ট এর মিনিমাম পাঁচটা প্রপার্টি থাকবে। তারপর একটা প্রপার্টি এর মান হবে অন্য আরেকটা অবজেক্ট , আরেকটা প্রপার্টি এর মান হবে একটা array । একটা প্রপার্টি এর মান হবে একটা ফাংশন(মেথড) । এই মেথড কে কল করলে সে মেথড এর ভিতর থেকে অবজেক্ট এর যেকোন একটা প্রপার্টি রিটার্ন করবে। আর বাকি দুইটা তোমার ইচ্ছা মতো। 
+
+const student = {
+    id: 101,
+    name: 'Protik',
+    profession: {job: 'student', schoolName: 'OIS', address: 'Tarail'},
+    subjects: ['Bangla', 'English', 'Math', 'Science'],
+    takeExam: function(name){
+        console.log(this.name,'taking exam');
+    }
+}
+// student.takeExam();
+
+// ২. একটা টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করো। সেটার মধ্যে উপরের অবজেক্ট এর একটা তিনটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। এর মধ্যে একটা প্রপার্টি সেট করবে--যেটা নেস্টেড অবজেক্ট আছে সেটার প্রপার্টি থেকে। আরেকটা প্রপার্টি হবে যে প্রপার্টি এর মান একটা array সেটার সেকেন্ড পজিশন এর উপাদান দিয়ে। আর বাকি একটা তোমার ইচ্ছা মতো বসাতে পারো। 
+
+const work = `${student.profession.job}`;
+// console.log(work);
+const subject = `${student.subjects[1]}`;
+// console.log(subject);
+// const exam = `${student.takeExam()}`;
+
+//৩.১. শূন্য প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে যেটা ৮৯ রিটার্ন করবে। 
+
+const sum = () => console.log(89) ;
+// sum();
+
+//৩.২. এক প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করবে। এই ফাংশনের কাজ হবে যে প্যারামিটার নিবে সেটাকে ৭ দিয়ে ভাগ করে ভাগফল রিটার্ন করবে। 
+
+const divide = num => num / 7;
+// console.log(divide(21));
+
+//৩.৩ দুই, প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। এই ফাংশনের কাজ হবে যে দুইটা প্যারামিটার ইনপুট নিবে। সেই দুইটা প্যারামিটারকে যোগ করে যোগফলকে দুই দিয়ে ভাগ করে ভাগফল রিটার্ণ করে দাও। 
+
+const addDivide = (num1, num2) => (num1 + num2)/2 ;
+// console.log(addDivide(4, 6));
+
+// ৩.৪ একাধিক লাইন ওয়ালা অ্যারো ফাংশন লিখো। সেটাতে দুইটা প্যারামিটার নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ৭ যোগ করবে তারপর যোগফল দুইটাকে আবার যোগ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const result = (num1, num2) => {
+    const addNum1 = num1 + 7;
+    const addNum2 = num2 + 7;
+    const sum = addNum1 + addNum2 ;
+    return sum;
+}
+// console.log(result(4, 5));
+
+//৪. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৭ দিয়ে ভাগ করে ভাগফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+const numbers = [7, 14, 21, 28, 35, 42, 49];
+const output = numbers.map(num => num/7);
+// console.log(output);
+
+//৫. জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+
+// ৬. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর সেকেন্ড পজিশন এর উপাদান কে destructuring করে 'balance' নামক একটা ভেরিয়েবল এ রাখবে।
+
+const student2 = {
+    id: 101,
+    name: 'Protik',
+    profession: {job: 'student', schoolName: 'OIS', address: 'Tarail'},
+    subjects: ['Bangla', 'English', 'Math', 'Science'],
+    takeExam: function(name){
+        console.log(this.name,'taking exam');
+    }
+}
+
+const {job, schoolName} = student.profession;
+const {subjects} = student;
+// console.log(job, schoolName);
+// console.log(subjects[1]);
